0c9de5e6d474dccc714f0c5be6ef5c1ce0fe7949
adji4 <sambadjianta@gmail.com>
2025-06-12 16:54:59 +0000

    maj

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     {
       title: 'Inception',
       description: 'Un thriller de science-fiction',
-      posterURL: 'https://image.tmdb.org/t/p/w200/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg',
+      posterURL: 'https://th.bing.com/th/id/R.ce272c7dd4bf3f749971df5209017c8a?rik=4ECIkv71ijKRmw&riu=http%3a%2f%2fimages2.fanpop.com%2fimage%2fphotos%2f14300000%2fInception-inception-2010-14355478-1680-1050.jpg&ehk=ZAHgjIbQdizdfXHDw%2bhEM64h3yM8gvSemKQu2%2fWN43M%3d&risl=&pid=ImgRaw&r=0.jpg',
       rating: 8.8
     },
     {
